534f08ca8d1cb4fdcf0b810b754cfe1a051a1fd6
Leonardo Erazo Martínez <leodan2003@gmail.com>
2025-04-02 11:50:30 -0600

    Acertijo 2 y 3 empezados pero no terminados

---
+++ ArmasMitologicas.pl
@@ -0,0 +1,7 @@
+Las armas de los heroes mitologicos (Aquiles, Perseo, Hercules y Teseo)
+poseen armas unicas (espada, lanza, arco y escudo) sabemos que :
+Aquiles no usa el escudo ni el arco
+Perseo no usa la espada 
+Hercules no usa la lanza ni el escudo
+Teseo usa el arco o escudo 
+que arma pertenece a cada heroe?
---
+++ GemasTitanes.pl
@@ -0,0 +1,5 @@
+(Cronos, Oceano, Hiperion y Japeto) tienen cada uno una gema (zafiro, rubi, esmeralda y diamante), sabemos que:
+Cronos no tiene el zafiro ni el diamante.
+Oceano no tiene el rubi
+Hiperion no tiene el zafiro ni el rubi
+Japeto tiene el diamante o la esmeraldaque gema tiene cada quien ?
